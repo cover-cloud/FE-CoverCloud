598d7c866bb71ca46348a1a20b9717ca9206d0f7
thseogns <thseo1758@naver.com>
2026-01-15 16:51:45 +0900

    빌드오류 수정

--- src/app/auth/callback/page.tsx
+++ src/app/auth/callback/page.tsx
@@ -5,6 +5,8 @@ import { useRouter, useSearchParams } from "next/navigation";
 import { completeLogin } from "@/app/utils/auth.service";
 import { useSnackbarStore } from "@/app/store/useSnackbar";
 
+export const dynamic = "force-dynamic";
+
 export default function CallbackPage() {
   const router = useRouter();
   const params = useSearchParams();
