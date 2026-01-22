489ddbf6a60a1d2dce0e7ad37297637dad3323a9
thseogns <thseo1758@naver.com>
2026-01-22 13:26:16 +0900

    테스트용 엑세스 토큰

--- src/app/api/cover/list.ts
+++ src/app/api/cover/list.ts
@@ -18,12 +18,16 @@ const fetchPopularCoverList = async ({
 }: FetchPopularCoverParams) => {
   const res = await axios.get(
     `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/cover/trending/search`,
+
     {
       params: {
         page,
         size,
         period: period === "ALL" ? undefined : period,
         genres,
+      },
+      headers: {
+        Authorization: `Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIyIiwidXNlcklkIjoyLCJ0eXBlIjoiYWNjZXNzIiwiaWF0IjoxNzY5MDU1ODc4LCJleHAiOjE3NjkwNTk0Nzh9._smnYkjuBq6g9u2IHuUZlml6dnObLHRBKpe9dsNGxn-abJIqfOrahqz3jOTXik4OBPgLDc-MKl9k5Lev0YqREw`,
       },
     },
   );
