b28d84ac0052f47bdf438c794c6a265347f77c39
thseogns <thseo1758@naver.com>
2026-01-29 15:45:03 +0900

    엑세스 토큰 제거

--- src/app/store/useAuthStore.ts
+++ src/app/store/useAuthStore.ts
@@ -8,8 +8,7 @@ interface AuthStore {
 
 export const useAuthStore = create<AuthStore>((set) => ({
   isLogin: false,
-  accessToken:
-    "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIyIiwidXNlcklkIjoyLCJ0eXBlIjoiYWNjZXNzIiwiaWF0IjoxNzY5NjY3NjQ5LCJleHAiOjE3Njk2NzEyNDl9.d3-yPu3Di4IhH0-CWPUFP3PHks_KcAnGRov95KMg406oipLucaemiu1EbN8XAWZMp18ShuBuSDq8BNZ3sALoYw",
+  accessToken: "",
   userId: null,
   setIsLogin: (isLogin: boolean) => set({ isLogin }),
   setAccessToken: (accessToken: string) => set({ accessToken }),
