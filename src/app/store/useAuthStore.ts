e4fe3741cb2d8ee4548f79a953d855e9905e55ba
thseogns <thseo1758@naver.com>
2026-01-30 13:54:05 +0900

    사운드클라우드 높이조정

--- src/app/store/useAuthStore.ts
+++ src/app/store/useAuthStore.ts
@@ -8,8 +8,7 @@ interface AuthStore {
 
 export const useAuthStore = create<AuthStore>((set) => ({
   isLogin: false,
-  accessToken:
-    "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIyIiwidXNlcklkIjoyLCJ0eXBlIjoiYWNjZXNzIiwiaWF0IjoxNzY5NzQ0MjgwLCJleHAiOjE3Njk3NDc4ODB9.DCmarQDE2ggnS1O7eZQ6Lfok1E5-YyB1YD9VubjYsbXdh0dc6O-VcwYV_8-ZKlVe2axvARFXvLLyyi0Zh-pxWw",
+  accessToken: "",
   userId: null,
   setIsLogin: (isLogin: boolean) => set({ isLogin }),
   setAccessToken: (accessToken: string) => set({ accessToken }),
