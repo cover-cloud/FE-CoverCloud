07c364753bf2368eadb6fc56be713b04c0eacd22
thseogns <thseo1758@naver.com>
2026-02-13 13:46:19 +0900

    변경시 호출

--- src/app/store/useAuthStore.ts
+++ src/app/store/useAuthStore.ts
@@ -8,8 +8,7 @@ interface AuthStore {
 
 export const useAuthStore = create<AuthStore>((set) => ({
   isLogin: false,
-  accessToken:
-    "eyJhbGciOiJIUzM4NCJ9.eyJzdWIiOiIyIiwidXNlcklkIjoyLCJ0eXBlIjoiYWNjZXNzIiwiaWF0IjoxNzcwOTU2NDU1LCJleHAiOjE3NzA5NjAwNTV9.UWGu4l_6j_A4bo9GlYKSEwPzZzCnfze1p6lHUJLxH7ZYevO1PUlwqMtUQJdxcWXe",
+  accessToken: "",
   userId: null,
   setIsLogin: (isLogin: boolean) => set({ isLogin }),
   setAccessToken: (accessToken: string) => set({ accessToken }),
