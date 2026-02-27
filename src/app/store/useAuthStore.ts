4339facbf4476270741ce1d3b640cdf7e7e13e59
thseogns <thseo1758@naver.com>
2026-02-27 17:12:48 +0900

    토큰제거

--- src/app/store/useAuthStore.ts
+++ src/app/store/useAuthStore.ts
@@ -8,8 +8,7 @@ interface AuthStore {
 
 export const useAuthStore = create<AuthStore>((set) => ({
   isLogin: false,
-  accessToken:
-    "eyJhbGciOiJIUzM4NCJ9.eyJzdWIiOiIyIiwidXNlcklkIjoyLCJ0eXBlIjoiYWNjZXNzIiwiaWF0IjoxNzcxOTk1MTU1LCJleHAiOjE3NzE5OTg3NTV9.egP4egPuK-kJMq26wUR0berTn6ynrTD8QWxCiQqeHoa-KA-elLiOnKNISulCYFv0",
+  accessToken: "",
   userId: null,
   setIsLogin: (isLogin: boolean) => set({ isLogin }),
   setAccessToken: (accessToken: string) => set({ accessToken }),
