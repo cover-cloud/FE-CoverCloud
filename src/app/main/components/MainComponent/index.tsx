4fedbf12338ddb3de3e89be5eb5e98c62aec630f
thseogns <thseo1758@naver.com>
2026-04-15 18:33:04 +0900

    모바일 호버상태 수정

--- src/app/main/components/MainComponent/index.tsx
+++ src/app/main/components/MainComponent/index.tsx
@@ -115,6 +115,11 @@ const MainComponent = ({ initialData }: { initialData?: any }) => {
     minWidth: "60px",
     minHeight: "32px",
     fontSize: "20px",
+    "@media (hover: none)": {
+      "&:hover": {
+        backgroundColor: "transparent",
+      },
+    },
     "@media (hover: hover)": {
       "&:hover": {
         backgroundColor: theme.palette.gray.secondary,
@@ -132,10 +137,14 @@ const MainComponent = ({ initialData }: { initialData?: any }) => {
     minHeight: "32px",
     padding: "0 12px",
     fontSize: "14px",
+    "@media (hover: none)": {
+      "&:hover": {
+        backgroundColor: "transparent",
+      },
+    },
     "@media (hover: hover)": {
       "&:hover": {
-        backgroundColor: theme.palette.genre.secondary,
-        color: theme.palette.common.black,
+        backgroundColor: theme.palette.gray.secondary,
       },
     },
   });
